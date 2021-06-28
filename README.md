0555d6c414292f4c232615f32520ff7225caced9
Mohammad Za'areer <zaa92reer@gmail.com>
2021-06-29 00:00:08 +0300

    setting up repo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # storefront
+
 e-Commerce storefront that will allow our users to browse our product offerings by category, place items in their shopping cart, and check-out when they are ready to make their purchase.
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import App from './app';
+
+function Main() {
+    return(
+        <>
+            <App />
+        </>
+    );
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
